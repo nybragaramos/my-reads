7913b21b449fcc4c4aa9337164838caff743a27b
nadjayanna <nadjayannabr@gmail.com>
2018-10-17 12:08:16 +0200

    add search functionality

--- src/components/search/Search.js
+++ src/components/search/Search.js
@@ -1,34 +1,64 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
-import PropTypes from 'prop-types';
+import Book from '../book/Book'
+import * as BooksAPI from './../../utils/BooksAPI'
 import './Search.css';
 
 class Search extends Component {
 
 	state = {
-		query: ''
+		query: '',
+		showingBooks: []
+	}
+
+	updateSearch = (query) => {
+
+		/*Adjust Query to not allow double space*/
+		let newQuery = query.split(' ');
+		newQuery = newQuery.filter(function(str) {
+    		return /\S/.test(str);
+		});
+		query = newQuery.join(' ');
+
+		this.updateQuery(query);
+
+		if(query.length > 0) {
+			BooksAPI.search(query).then((books) => {
+				if(books.length > 0) {
+					this.updateBooks(books);
+				} else {
+					this.clearQuery();
+				}
+			})
+		} else {
+				this.clearQuery();
+		}
 	}
 
 	updateQuery = (query) => {
-		this.setState({ query: query.trim() });
+		this.setState({ query: query });
+	}
+
+	updateBooks = (books) => {
+		this.setState({ showingBooks: books});
 	}
 
 	clearQuery =  () => {
-		this.setState({ query: ''});
+		this.setState({ showingBooks: []});
 	}
 
 	render() {
-		
-		let showingBooks;
 
 		return (
 			<div>
 				<div className='search-bar'>
 					<Link to='/' className='search-close'>Close</Link>
-					<input className='search-books' type='text' placeholder='Search books by title or author' value={this.state.query} onChange= {(event) => this.updateQuery(event.target.value)}/>
+					<input className='search-books' type='text' placeholder='Search books by title or author' value={this.state.query} onChange= {(event) => this.updateSearch(event.target.value)}/>
 				</div>
 				<div className='search-results'>
-					<p>Search</p>
+					<ol className='shelf-books'>
+						{this.state.showingBooks.map(book => <li key={book.id}><p style={{ width: 100, height: 193 }}>{book.title}</p>{/*console.log(book)*/}</li>)}
+					</ol>
 				</div>
 			</div>
 		);
